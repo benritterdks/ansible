58814a43f16ee6037b852fdc79099a41506b0468
Ben Ritter <benjamin.ritter@dcsg.com>
2022-05-22 23:33:27 -0400

    Added brew, asdf, zsh setups

---
+++ install.sh
@@ -0,0 +1,23 @@
+command -v brew
+if [[ $? != 0 ]] ; then
+	echo "Install homebrew through self service before continuing"
+	exit 1
+else
+	echo "Homebrew installed"
+fi
+
+SSH=~/.ssh/id_ed25519
+if [[ ! -f "$SSH" ]] ; then
+	echo "Setup github SSH key to continue script"
+	exit 1
+else
+	echo "SSH key found"
+fi
+
+# Install ansible
+echo "Installing ansible..."
+brew install ansible
+
+# Install community.general collection
+echo "Installing community.general..."
+ansible-galaxy collection install community.general
